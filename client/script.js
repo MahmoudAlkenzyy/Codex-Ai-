2616525882aafd15875c8d7455b7627755dcdbf0
MahmoudAlkenzyy <105129954+MahmoudAlkenzyy@users.noreply.github.com>
2024-01-01 22:24:08 +0200

    #Change the Ai Model

--- client/script.js
+++ client/script.js
@@ -71,7 +71,7 @@ const handleSubmit = async (e) => {
     loader(messageDiv);
 
     // fetch data
-    const response = await fetch('http://localhost:5000/', {
+    const response = await fetch('https://codex-ai-qinx.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
